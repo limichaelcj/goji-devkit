353879e35d238333d15eaada7b4ba8a36e5545c6
Michael Li <mike830@gmail.com>
2019-03-04 15:57:23 -0700

    make preliminary changes to photogallery.js

--- components/PhotoGallery.js
+++ components/PhotoGallery.js
@@ -1,83 +1,35 @@
 export default class PhotoGallery {
-  constructor(images,node){
-    this._node = null; //parent node
-    this._galleryNode = null; //container node .PhotoGallery
-    this._images = images; //images paths in array
-    this._style = null; //css style object
-    //if parent node included in constructor, build automatically
-    if(node){
-    	this.build(node);
+  constructor(node, options = {}){
+    if (!node || !(node instanceof HTMLElement)){
+      throw new Error('ArgumentError: Constructor requires argument of type HTMLElement')
     }
-  }
-  set style(obj){
-    this._style = obj;
+    this._node = node;
+    this._photos = Array.from(node.querySelectorAll('img')); //images paths in array
+    this._settings = {
+      direction: 'row',
+      padding: 50,
+      color: 'white'
+    }
+    this._style = {
+      display: 'flex',
+
+    }
   }
   get node() {
   	return this._node;
   }
-  get galleryNode(){
-    return this._galleryNode;
-  }
-  get images() {
-    return this._images;
+  get photos() {
+    return this._photos;
   }
   get style(){
     return this._style;
   }
-  addImage(img) {
-    this._images.push(img);
-  }
-  clear(){
-    this._images = null;
-  }
-  build(node){
-    //clear previous node, if any
-    if(this.node){
-      while(this.node.firstChild){
-        this.node.firstChild.remove();
-      }
-    }
-    //change node
-  	this._node = node;
-    //clear node children
-    while(node.firstChild){
-    	node.firstChild.remove();
-    }
-    //create container
-    let container = document.createElement('div');
-    container.className = "PhotoGallery";
-    //create elements
-  	for(let i = 0; i < this.images.length; i++){
-    	let div = document.createElement('div');
-      let img = document.createElement('img');
-      //inject img path from this.images
-      img.src = this.images[i];
-      div.append(img);
-      container.append(div);
-    }
-    //append component to target node
-    node.append(container);
-    //add to object property data
-    this._galleryNode = container;
-    //set optional styles
-    this.applyStyle();
+
+  configure(options){
+    
   }
 
-  applyStyle(obj){
-    //overwrites instance's style if argument passed
-    if (obj){
-      this.style = obj;
-    }
-    //apply styles only if node and style exists
-    console.log(this.style);
-    if(this.galleryNode && this.style){
-      for(let key in this.style){
-        this.galleryNode.style[key] = this.style[key];
-      }
-    }
-    /* CAUTION:
-      This method does not erase previously set styles.
-      Rebuild the instance with build(node) to reset styles.
-    */
+  initialize(){
+
   }
 }
