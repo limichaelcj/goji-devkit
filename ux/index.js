0e386ad588e8abe46d5b48e8ca0cb54a0741dab8
Michael Li <mike830@gmail.com>
2019-03-11 14:56:47 -0600

    add smoothScrollJQuery function

--- ux/index.js
+++ ux/index.js
@@ -2,4 +2,4 @@ const dragElement = require('./dragElement');
 const smoothScroll = require('./smoothScroll');
 const scrollFade = require('./scrollFade');
 
-module.exports = { dragElement, smoothScroll, scrollFade };
+module.exports = { dragElement, smoothScroll, smoothScrollJQuery, scrollFade };
