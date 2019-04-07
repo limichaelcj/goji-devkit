86ba6fb2bea39891959ac4ed453668d7c2e166b3
Michael Li <mike830@gmail.com>
2019-04-06 19:56:11 -0600

    test functional package require

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-const components = require('./components/index');
-const data = require('./data/index');
-const ui = require('./ui/index');
-const ux = require('./ux/index');
-
 module.exports = {
-  () => require('./components/index'),
-  () => require('./data/index'),
-  () => require('./ui/index'),
-  () => require('./ux/index')
+  components: () => require('./components/index'),
+  data: () => require('./data/index'),
+  ui: () => require('./ui/index'),
+  ux: () => require('./ux/index')
 }
