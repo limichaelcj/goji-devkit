a66b7dfc6cae5cb137bc6ed9fc5e83e894273b79
Michael Li <mcli.893@gmail.com>
2019-04-12 15:08:18 -0600

    fix function rename in module.export tree

--- ux/index.js
+++ ux/index.js
@@ -1,5 +1,5 @@
 const dragElement = require('./dragElement');
-const imageLoader = require('./imageLoader');
+const imageLoader = require('./assetLoader');
 const smoothScroll = require('./smoothScroll');
 const smoothScrollJQuery = require('./smoothScroll');
 const scrollFade = require('./scrollFade');
