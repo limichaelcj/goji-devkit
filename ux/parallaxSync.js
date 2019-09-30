e707a6a078a2a605b5e7d597d10ec8e8fcec3bcd
Michael Li <39263631+mcli830@users.noreply.github.com>
2019-09-30 14:53:32 -0700

    update parallaxSync.js

--- ux/parallaxSync.js
+++ ux/parallaxSync.js
@@ -1,26 +1,50 @@
-module.exports = function(anchorSelector, linkSelector){
-  const anchor = document.querySelector(anchorSelector);
-  const links = Array.from(document.querySelectorAll(linkSelector));
-  links.forEach(el => Object.assign(el.style, {
-    position: 'sticky',
-    top: 0
-  }));
+module.exports = function(selector){
+  
+  // get selected elements and sort descending by height
+  const elements = [...document.querySelectorAll(selector)];
+  const descHeight = elements.sort((a,b) => b.getBoundingClientRect().height - a.getBoundingClientRect().height);
+  
+  // anchor, the tallest element, will scroll normally
+  // links, all other elements, will scroll slower
+  const [ anchor, ...links ] = descHeight;
+  
+  // this function's css transforms leverage position and top rules
+  links.forEach(el => {
+    Object.assign(el.style, {
+      position: 'sticky',
+      top: 0,
+    })
+  })
+  
+  // add window scroll listener
   window.addEventListener('scroll', e => {
-    const rect = anchor.getBoundingClientRect();
-    // check if the element is above the client's viewport
-    if (rect.top < 0){
-      const ratio = rect.top / rect.height * -1;
-      const percent = ratio > 1 ? 1 : ratio;
+    
+    const anchorRect = anchor.getBoundingClientRect();
+    
+    // if anchor top is above viewport, apply parallax sync
+    if (anchorRect.height > window.innerHeight && anchorRect.top < 0){
+      
+      // the ratio of pixels that the client is below the anchor's top
+      // will be used to calculate the offset of every link's top
+      const ratio = anchorRect.top / anchorRect.height;
+      
+      // iterate each link that is smaller than the anchor
       links.forEach(link => {
         const linkRect = link.getBoundingClientRect();
-        if (window.innerHeight < linkRect.height){
-          const relativeHeight = linkRect.height - window.innerHeight;
-          const offset = ratio * relativeHeight;
-          link.style.top = `-${offset}px`
-        } else {
-          link.style.top = '1em'
+        
+        // only apply parallax if the height is greater than the viewport
+        // and the link's bottom is below the viewport bottom
+        if (linkRect.height > window.innerHeight && window.innerHeight < linkRect.bottom){
+          
+          // apply ratio to the link height - viewport height,
+          // matching the link's bottom to anchor's bottom
+          const diff = linkRect.height - window.innerHeight;
+          const offset = ratio * diff;
+          
+          // apply the css transform which will be a negative px value
+          link.style.top = `${offset}px`;
         }
       })
     }
-  })
+  });
 }
